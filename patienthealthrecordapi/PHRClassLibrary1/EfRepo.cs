d16127e5e3cbe2d5cbcfaf56bece9dc41c4f2354
slickcharmer <bkentee741@gmail.com>
2023-04-01 16:28:20 +0530

    added two new endpoints in PHR api

--- patienthealthrecordapi/PHRClassLibrary1/EfRepo.cs
+++ patienthealthrecordapi/PHRClassLibrary1/EfRepo.cs
@@ -236,5 +236,95 @@ namespace EntityFrame
             return test;
         }
 
+        public List<All> GetAll(string pid)
+        {
+            var hr = _PHdbcontext.PatientHealthRecords;
+            var tr = _PHdbcontext.PatientTests;
+            var mr = _PHdbcontext.PatientMedications;
+            var ar = _PHdbcontext.PatientAllergies;
+            var br = _PHdbcontext.PatientBasicRecords;
+            var res = (from h in hr
+                       where h.PatientId == pid
+                       select new PHRModels.All()
+                       {
+                           MoreInfo = (from hh in hr
+                                       where hh.PatientId == h.PatientId && hh.AppointmentId == h.AppointmentId
+                                       select new Models.Patient_Health_Record()
+                                       {
+                                           Doctor_Id = hh.DoctorId,
+                                           Date_Time = hh.DateTime,
+                                           Conclusion = hh.Conclusion,
+                                           Appointment_Id = hh.AppointmentId,
+                                       }).ToList(),
+                           Allergys = (from a in ar
+                                       where a.HealthId == h.PatientId && a.AppointmentId == h.AppointmentId
+                                       select new Models.Patient_Allergy()
+                                       {
+                                           Allergy = a.Allergy,
+                                           Appointment_Id = a.AppointmentId
+                                       }).ToList(),
+                           Basic = (from b in br
+                                    where b.PatientId == h.PatientId && b.AppointmentId == h.AppointmentId
+                                    select new Models.Patient_Basic_Record() {
+                                        Date_Time = b.DateTime,
+                                        Appointment_Id = b.AppointmentId,
+                                        Bp = b.Bp,
+                                        Heart_Rate = (int)b.HeartRate,
+                                        SpO2 = b.SpO2,
+                                        Height = b.Height,
+                                        Weight = b.Weight,
+                                        BloodGroup = b.BloodGroup,
+                                        Temperature = b.Temperature,
+                                    }).ToList(),
+                           Tests = (from t in tr
+                                    where t.HealthId == h.PatientId && t.AppointmentId == h.AppointmentId
+                                    select new Models.Patient_Test()
+                                    {
+                                        Test = t.Test,
+                                        Result = t.Result,
+                                        Appointment_Id = t.AppointmentId
+                                    }).ToList(),
+                           Drugs = (from m in mr
+                                    where m.HealthId == h.PatientId && m.AppointmentId == h.AppointmentId
+                                    select new Models.Patient_Medication()
+                                    {
+                                        Drugs = m.Drug,
+                                        Quantity = m.Quantity,
+                                        Appointment_Id = m.AppointmentId
+                                    }).ToList(),
+                       }).ToList();
+            return res;
+        }
+
+        public List<PHRModels.BasicsAll> BasicsAlls(string pid, string aid)
+        {
+            var ar = _PHdbcontext.PatientAllergies;
+            var br = _PHdbcontext.PatientBasicRecords;
+            var res = (from b in br
+                       where b.PatientId == pid && b.AppointmentId == aid
+                       select new PHRModels.BasicsAll()
+                       {
+                           DateTime = b.DateTime,
+                           PatientId = b.PatientId,
+                           AppointmentId = b.AppointmentId,
+                           Bp = b.Bp,
+                           HeartRate = b.HeartRate,
+                           SpO2 = b.SpO2,
+                           Height = b.Height,
+                           Weight = b.Weight,
+                           BloodGroup = b.BloodGroup,
+                           Temperature = b.Temperature,
+
+                           Allergies = (from a in ar
+                                        where a.HealthId == b.PatientId && a.AppointmentId == b.AppointmentId
+                                        select new Models.Patient_Allergy()
+                                        {
+                                            Allergy = a.Allergy,
+                                            Appointment_Id = a.AppointmentId,
+                                            Health_Id = a.HealthId,
+                                        }).ToList()
+                       }).ToList();
+            return res;
+        }
     }
 }
--- patienthealthrecordapi/PHRModels/All.cs
+++ patienthealthrecordapi/PHRModels/All.cs
@@ -2,10 +2,40 @@
 {
     public class All
     {
-        public IEnumerable<Models.Patient_Basic_Record> Basics { get; set; }
-        public IEnumerable<Models.Patient_Health_Record> Detailed { get; set; }
+        /*public string doctorId { get; set; }
+        public string date { get; set; }
+        public string conclusion { get; set; }
+        public string appointment_id { get; set; }*/
+        public IEnumerable<Models.Patient_Health_Record>MoreInfo{ get; set; }
         public IEnumerable<Models.Patient_Test> Tests { get; set; }
         public IEnumerable<Models.Patient_Medication> Drugs { get; set; }
-        public IEnumerable<Models.Patient_Allergy> Allergy { get; set; }
+        public IEnumerable<Models.Patient_Allergy> Allergys { get; set; }
+        public IEnumerable<Models.Patient_Basic_Record> Basic { get; set; }
+
+    }
+
+    public class BasicsAll
+    {
+        public DateTime? DateTime { get; set; }
+
+        public string? PatientId { get; set; }
+
+        public string AppointmentId { get; set; } = null!;
+
+        public string? Bp { get; set; }
+
+        public int? HeartRate { get; set; }
+
+        public string? SpO2 { get; set; }
+
+        public string? Weight { get; set; }
+
+        public string? Height { get; set; }
+
+        public string? BloodGroup { get; set; }
+
+        public string? Temperature { get; set; }
+
+        public IEnumerable<Models.Patient_Allergy> Allergies {get; set; }
     }
 }
